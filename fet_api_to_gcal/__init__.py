0120ce548b9d47ed8cd2f61d3163ccb0eaac68c7
omega-coder <fy_cherief@esi.dz>
2019-07-29 13:52:42 +0200

    Fixes config import

--- fet_api_to_gcal/__init__.py
+++ fet_api_to_gcal/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-import fet_api_to_gcal.config
+from fet_api_to_gcal import config
 
 app = Flask(__name__)
 app.config.from_object(config.DevelopmentConfig)
