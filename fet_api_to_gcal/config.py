84be0991860e0c74ea0c9f9b3faf84228bc5bc60
omega-coder <fy_cherief@esi.dz>
2019-07-30 19:42:19 +0200

    Adds CLIENT_ID and CLIENT_SECRET to config

--- fet_api_to_gcal/config.py
+++ fet_api_to_gcal/config.py
@@ -1,13 +1,14 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
-
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+    CLIENT_ID = "1008498480829-sqolhtujjmaelhot5pdrk82o5qj9hige.apps.googleusercontent.com"
+    CLIENT_SECRET = "uzWWHqekA-0mFO3wBBPlJw6V"
+
 
 class ProductionConfig(Config):
     DEBUG = False
