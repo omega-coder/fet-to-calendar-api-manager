ddb9daee2f1ad116c2244da7497a33009f505ddb
omega-coder <fy_cherief@esi.dz>
2019-08-02 22:14:40 +0200

    adds environement variables to work with http protocol

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+import os
 from fet_api_to_gcal import app
+from fet_api_to_gcal import db
+# set envionment variables
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "1"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
