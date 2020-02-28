835c39f97ef68e86d285c21f2121282db93cf369
CHERIEF Yassine <fy_cherief@esi.dz>
2020-02-28 04:55:39 +0100

    Add proper a logging system

--- remove_events_from_tt.py
+++ remove_events_from_tt.py
@@ -1,5 +1,10 @@
 import argparse
+import logging
+import ntpath
 import sys
+
+from fet_api_to_gcal.common.utils import timestamped_filename
+from fet_api_to_gcal.common.utils import path_leaf
 
 
 def main(filepath, event_names):
@@ -9,28 +14,51 @@ def main(filepath, event_names):
         filepath (str): path for the FET generated timetable csv file.
         event_names (list): a list containing the event names (string values) to be deleted. 
     """
-    f = open(filepath, "r")
-    data = f.readlines()
 
-    # open a file for the resulting filtered file
+    log_filename = timestamped_filename(filename=path_leaf(filepath))
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%m-%d %H:%M:%S',
+                        filename="./logs/{}.log".format(log_filename),
+                        filemode='w')
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.WARNING)
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+    
+    logging.info("[?] Opening file {}".format(path_leaf(filepath)))
+    try:
+        f = open(filepath, "r")
+        data = f.readlines()
+    except Exception as e:
+        logging.exception("[!] Exception while opening file {}".format(filepath))
+        logging.error("[!] Exiting script")
+        sys.exit(1)
+    # ? open a file for the resulting filtered file
 
     try:
         result_file = open(
             "{}_REMOVED_{}".format(filepath, "".join(event_names)), "w")
     except Exception as e:
-        print("Exception while opening result file: {}".format(e))
+        logging.error("[!] Exception while opening result file: {}".format(e))
         sys.exit(1)
 
     # write the first line of the csv containing columns
+    logging.info("[?] started writing data to result file.")
     result_file.write(data[0])
-
+    counter = 0
     for line in data[1:]:
         event__name__ = line.split(",")[4].replace('"', '')
         if event__name__ in event_names:
+            counter += 1
             continue
         else:
             result_file.write(line)
-
+    logging.info("[+] Finished writing to result file")
+    logging.info("Removed {} event records from original file".format(counter))
 
 if __name__ == "__main__":
     # create an arguemnt parser
@@ -46,9 +74,11 @@ if __name__ == "__main__":
                         "--event",
                         dest="event_names",
                         action='append',
-                        default=[])
+                        default=[],
+                        help="event name to be appended to the list of event names")
 
     # Parse arguments
+    
     args = parser.parse_args()
     # call the main function
     main(args.file, args.event_names)
